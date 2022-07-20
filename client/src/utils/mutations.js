7f838208d07be1b541451fc52d760d4ddf33c6f2
yuval7994 <yuvalba45@gmail.com>
2022-07-19 21:12:57 -0500

    ignore node modules and add other files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from "@apollo/client"
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+      }
+    }
+  }
+`
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: BookInput) {
+    saveBook(book: $book) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        title
+        bookId
+      }
+    }
+  }
+`
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        title
+        bookId
+      }
+    }
+  }
+`
