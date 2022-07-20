4a90ffff99bf4509dd9de82fb7c778364707dfbe
yuval7994 <yuvalba45@gmail.com>
2022-07-19 20:47:59 -0500

    all files

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require("apollo-server-express")
+
+// TODO: Add input type for saveBook
+const typeDefs = gql`
+  input BookInput {
+    authors: [String]
+    title: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookInput): User
+    removeBook(bookId: String!): User
+  }
+`
+module.exports = typeDefs
